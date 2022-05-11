75cb448e0612cea4023bad834ef29b3404c15018
CrAzI-code <emmaobiora96@gmail.com>
2022-05-10 19:00:58 -0700

    just completed login and registration page

--- birth_certificate/urls.py
+++ birth_certificate/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
-
+app_name = "birth_certificate"
 urlpatterns = [
     path('', views.index, name= "index"),
-]
+    path('about', views.about, name="about"),
+    path('gallery/', views.gallery, name="gallery"),
+    path('contact/', views.contact, name="contact"),    
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
+    ]
--- birth_certificate/views.py
+++ birth_certificate/views.py
@@ -5,3 +5,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, 'about.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return render(request, 'logout.html')
