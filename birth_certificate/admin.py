184f9b8b7eb40efc1debf6fefaec1a4d2c7a7346
Naman Baranwal <namanbaranwal2002@gmail.com>
2022-05-17 22:09:23 +0530

    1. Created Payment Token Model 2. Created Way to verify token 3. Updated View

--- birth_certificate/admin.py
+++ birth_certificate/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PersonalData
+from .models import PaymentToken, PersonalData
 
 # Register your models here.
+admin.site.register(PaymentToken)
 admin.site.register(PersonalData)
